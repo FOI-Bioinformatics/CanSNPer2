29036b69a3b2ebaa0fbea5357b8ae77688b47630
David Sundell <david.sundell@foi.se>
2020-04-23 10:43:36 +0200

    version update

--- CanSNPer2/CanSNPerTree.py
+++ CanSNPer2/CanSNPerTree.py
@@ -3,7 +3,7 @@
 CanSNPer2: A toolkit for SNP-typing NGS data.
 Copyright (C) 2019 David Sundell @ FOI bioinformatics group
 
-VERSION 2.0.2 release of CanSNPer2
+VERSION 2.0.3 release of CanSNPer2
 
 The second release of CanSNPer (CanSNPer2) is exclusively written for python3
 CanSNPer2 is simplified from CanSNPer1 stripped to only perform
@@ -29,13 +29,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 oname = __name__
 __name__ 		= "CanSNPer2"
-__version__ 	= "2.0.2"
+__version__ 	= "2.0.3"
 __author__ 		= "David Sundell"
 __credits__ 	= ["David Sundell"]
 __license__ 	= "GPLv3"
 __maintainer__ 	= "FOI bioinformatics group"
 __email__ 		= ["bioinformatics@foi.se", "david.sundell@foi.se"]
-__date__ 		= "2020-04-15"
+__date__ 		= "2020-04-23"
 __status__ 		= "Production"
 
 from CanSNPer2.modules.CanSNPer2 import CanSNPer2
